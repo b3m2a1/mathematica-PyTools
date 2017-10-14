657b8364fa93448272c103fa4b1def47ea76d7a7
b3m2a1 <b3m2a1@gmail.com>
2017-10-13 23:22:42 -0700

    Committed PyTools application @ 2017-10-13_23:22:42

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,7 +1,7 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
 Paclet[Location -> 
   "/Users/Mark/Documents/Wolfram Mathematica/Applications/PyTools", 
- Name -> "PyTools", Version -> "1.0.2", 
+ Name -> "PyTools", Version -> "1.0.3", 
  Extensions -> {{"Kernel", "Root" -> ".", "Context" -> {"PyTools`"}, 
     Root -> ".", Context -> {"PyTools`"}}, 
    {"Resource", "Root" -> "Resources", "Resources" -> {"MLib"}}}]
