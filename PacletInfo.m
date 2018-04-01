3790c559021c3f8669067c066eeb5f9d3094310e
b3m2a1 <b3m2a1@gmail.com>
2018-04-01 13:05:38 -0700

    Commited via Mathematica
    Committed PyTools application @ 2018-04-01_13:05:38

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,5 +1,5 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "PyTools", Version -> "1.0.7", Creator -> "b3m2a1@gmail.com", 
+Paclet[Name -> "PyTools", Version -> "1.0.8", Creator -> "b3m2a1@gmail.com", 
  Description -> "A process-based link between Mathematica and python.\n \
 Includes tools based off of this link", Tags -> {"mathematica", "python"}, 
  Extensions -> {{"Kernel", "Root" -> ".", "Context" -> {"PyTools`"}, 
